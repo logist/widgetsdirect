a903c1a0cc7e97d0102dc34c1a769582ad514d7b
logist <logist@github.com>
2017-08-29 16:07:08 +1000

    Updated build and docker files

--- WidgetsDirect/build.sh
+++ WidgetsDirect/build.sh
@@ -6,5 +6,5 @@ then
 fi
 
 printf "\nBuilding images stage $1...\n"
-docker build --no-cache -t idp:v$1 --build-arg ISC_PACKAGE_USER_PASSWORD="SYS" --build-arg ISC_PACKAGE_CSPSYSTEM_PASSWORD="SYS" .
+docker build -t wd:v$1 --build-arg ISC_PACKAGE_USER_PASSWORD="SYS" --build-arg ISC_PACKAGE_CSPSYSTEM_PASSWORD="SYS" .
 docker images 
--- WidgetsDirect/drun.sh
+++ WidgetsDirect/drun.sh
@@ -9,4 +9,5 @@ printf "\nStarting container...\n"
 #docker run -d -p 5900$1:57772 -p 5800$1:1972 --volume /data/idp:/usr/cachesys -h idp:v$1 --privileged --name idpv$1 idp:v$1
 docker run -d -p 5900$1:57772 -p 5800$1:1972 -h idp:v$1 --privileged -e ISC_DATA_DIRECTORY=/data/docker/SYS/IDP2017 --name idpv$1 idp:v$1
 docker container ls
-printf "\nYou can point your browser to http://localhost:5900$1/csp/sys/UtilHome.csp\n"
+printf "\nYou can point your browser to http://localhost:5900$1/csp/sys/UtilHome.csp for Management Protal\n"
+printf "\nYou can point your browser to http://localhost:5900$1/widgetsdirect/Welcome.csp for Widgets Direct\n"
